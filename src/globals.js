8ec770596d49fd3ace2154841c990e1c75c3f3eb
Uday Ghulaxe <ughulaxe@deloitte.com>
2022-12-23 22:34:50 +0530

    Updates

--- src/globals.js
+++
@@ -1,5 +0,0 @@
-export const Constants = {
-    GOOGLE_SCOPE: 'https://www.googleapis.com/auth/calendar',
-    GOOGLE_DISCOVERY_DOCS: 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'
-  };
-  
